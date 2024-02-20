383cdd8c16a137651ed2b3aec35f665f96c38df6
srundara <dara.srun168@gmail.com>
2024-02-20 15:07:59 +0700

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,27 +12,30 @@ const getItem = () => {
         }
     )
 }
+const setImg = (i,img) => {
+    console.log(data.value[i])
+    data.value[i] = img
+}
 onMounted(() => {
     getItem()
 })
 </script>
 <template>
-    <div class="container mt-3">
+    <div class="container mt-3 mb-3">
         <div class="row">
-            <div class="col-xxl-3 col-xl-3 col-lg-3 col-md-3 item-box" v-for="(item,i) in data" :key="i">
-                <div class="card">
-                    <div class="card-header">
+            <div class="col-xxl-3 col-xl-3 col-lg-3 col-md-3 item-box" v-for="(item,i1) in data" :key="i1">
+                <div class="box">
+                    <div class="img-box">
                         <img :src="item.img" alt="">
-
                     </div>
-                    <div class="card-body">
-                        <div class="title">
-                            <p>{{item.name}}</p>
-                            <p>{{item.price}}</p>
-                        </div>
-                        <div class="card-img">
-                            <img :src="item.imgList" alt="">
-                        </div>
+                    <div class="box-text">
+                        <h1>{{ item.name }}</h1>
+                        <h2>${{ item.price }}</h2>
+                        <ul>
+                            <li @click="setImg(i1,img)" :class="{'active':item.img==img}" v-for="(img,k) in item.imgList" :key="k">
+                                <img :src="img" alt="">
+                            </li>
+                        </ul>
                     </div>
                 </div>
 
